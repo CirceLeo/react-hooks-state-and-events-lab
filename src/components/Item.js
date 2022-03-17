05441dd6db224e22267b7fb82d1d8004df2e668b
Calypso Leonard <calypsorleonard@gmail.com>
2022-03-17 14:28:47 -0400

    commit tree 2c0d725b512e61c3a88de45a60c6cfbe80b11775

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function Item({ name, category }) {
+  const [isIn, setIn] = useState(false);
+  const itemClass = isIn ? "in-cart" : ""
+
+  function HandleItemClick(){
+    setIn((isIn)=>!isIn)
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={HandleItemClick}>{isIn ? "Remove From Cart":"Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import { render } from "@testing-library/react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [filterCategory, setFilterCategory] = useState("All")
+
+  function handleSelect(event){
+    setFilterCategory(event.target.value)
+  }
+  const filteredItems = items.filter((item) => {
+    if (filterCategory === "All"){return true}
+    else { return item.category === filterCategory}})
+
+  let renderedItems = filteredItems.map(item => <Item key={item.id} name={item.name} category={item.category} />)
+  
+  if (filterCategory === "All"){console.log(filterCategory)}
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleSelect} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {renderedItems}
       </ul>
     </div>
   );
